6d4c5155d78a935d118f0f3a955a31e9545fca46
Monica23-N <Monica20062309@gmail.com>
2026-02-22 20:02:53 +0530

    Initial porfolio project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend is working 🚀");
+});
+
+app.post("/contact", (req, res) => {
+    console.log(req.body);
+    res.json({ message: "Form received successfully ✅" });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});

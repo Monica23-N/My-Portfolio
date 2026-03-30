eae31872f5c8d5c78c48667c3877eb03ea11dc6e
Monica23-N <Monica20062309@gmail.com>
2026-03-30 19:53:22 +0530

    Fixed send button issue

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+
+//const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
+//mongoose.connect("mongodb+srv://monica20062309_db_user:01WTSeNKIkYdfl3k@cluster0.bxht8xp.mongodb.net/myDatabase?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority")
+//.then(() => console.log("DB Connected ✅"))
+//.catch(err => console.log(err));
 
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Backend is working 🚀");
